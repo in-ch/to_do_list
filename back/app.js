049d3df8c153452d7f9618f2a75d6b36f24221e5
inch <sxin2949@naver.com>
2021-03-21 20:07:54 +0900

    백엔드 세팅

---
+++ back/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const db = require('./models');
+
+// const postRouter = require('./routes/post');
+// const postsRouter = require('./routes/posts');
+// const userRouter = require('./routes/user');
+// const hashtagRouter = require('./routes/hashtag');
+const passportConfig = require('./passport');
+
+dotenv.config();
+const app = express();
+db.sequelize.sync()
+  .then(() => {
+    console.log('db 연결 성공');
+  })
+  .catch(console.error);
+passportConfig();
+
+
+app.use(morgan('dev'));
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(session({
+//   saveUninitialized: false,
+//   resave: false,
+//   secret: process.env.COOKIE_SECRET,
+// }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen('3065', () =>{
+    console.log('Hello');
+});
