32e744313d88e080d69331d207dac3903c733f9a
inch <sxin2949@naver.com>
2021-04-05 20:13:09 +0900

    pm2 설정

--- back/app.js
+++ back/app.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const db = require('./models');
+const hpp = require('hpp');
+const path = require('path');
+const helmet = require('helmet');
 
 // const postRouter = require('./routes/post');
 // const postsRouter = require('./routes/posts');
@@ -22,10 +25,16 @@ db.sequelize.sync()
   .catch(console.error);
 passportConfig();
 
-
+if(process.env.NODE_ENV === 'production'){
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
 app.use(morgan('dev'));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','incheolisbest.com'],
   credentials: true,
 }));
 app.use(express.json());
@@ -42,6 +51,10 @@ app.use(passport.session());
 
 
 app.use('/user', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
 
 app.listen('3065', () =>{
   console.log('Hello');
